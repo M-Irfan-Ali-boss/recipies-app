f607370f8af93a33cc7c10125e0ecc80a9763a64
M Irfan Ali <sheikhirfan@bearplex.com>
2021-05-11 19:16:10 +0500

    Completed the recipe app

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import CategoryGridTile from '../components/category/CategoryGridTile';
+import { useSelector } from 'react-redux';
 
-const CategoriesScreen = () => {
+const CategoriesScreen = ({ navigation }) => {
+	const { category } = useSelector((state) => state);
+	const renderGridItem = (itemData) => {
+		return (
+			<CategoryGridTile
+				title={itemData.item.title}
+				color={itemData.item.color}
+				handlerPress={() =>
+					navigation.navigate('CategoryMeal', {
+						categoryId: itemData.item.id,
+						categoryTitle: itemData.item.title,
+					})
+				}
+			/>
+		);
+	};
+
 	return (
-		<View>
-			<Text>Categories Screen</Text>
+		<View style={styles.screen}>
+			<FlatList
+				keyExtractor={(item) => item.id}
+				numColumns={2}
+				data={category.categories}
+				renderItem={renderGridItem}
+			/>
 		</View>
 	);
 };
 
 export default CategoriesScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		paddingVertical: 15,
+	},
+});
